e0045c9164c9393c20cc10d99fff8d0a56319706
jamesaq12wsx <jamesaq12wsx@gmail.com>
2016-06-02 15:21:11 +0800

    add LocationListTableViewCell and LocationListTableViewController

---
+++ ToGoList/ToGoList/LocationListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  LocationListTableViewCell.swift
+//  ToGoList
+//
+//  Created by 林紹瑾 on 2016/6/2.
+//  Copyright © 2016年 group7. All rights reserved.
+//
+
+import UIKit
+
+class LocationListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
