ef6726df52088f8c00397045a0df8a7e5ac0905a
Arick Chen <s885219s@gmail.com>
2016-06-07 13:16:41 +0800

    pin

---
+++ ToGoList/build/Pods.build/Debug-iphonesimulator/Pods-ToGoList.build/Objects-normal/x86_64/Pods-ToGoList-dummy.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/Arick/Documents/swift/To-Go-List/ToGoList/Pods/Target\ Support\ Files/Pods-ToGoList/Pods-ToGoList-dummy.m
---
+++ ToGoList/build/Pods.build/Debug-iphonesimulator/SQLite.swift.build/Objects-normal/x86_64/SQLite-Bridging.d
@@ -0,0 +1,5 @@
+dependencies: \
+  /Users/Arick/Documents/swift/To-Go-List/ToGoList/Pods/SQLite.swift/SQLite/Core/SQLite-Bridging.m \
+  /Users/Arick/Documents/swift/To-Go-List/ToGoList/Pods/Target\ Support\ Files/SQLite.swift/SQLite.swift-prefix.pch \
+  /Users/Arick/Documents/swift/To-Go-List/ToGoList/Pods/SQLite.swift/SQLite/Core/SQLite-Bridging.h \
+  /Users/Arick/Documents/swift/To-Go-List/ToGoList/Pods/SQLite.swift/SQLite/Core/fts3_tokenizer.h
